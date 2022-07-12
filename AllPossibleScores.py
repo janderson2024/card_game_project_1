2ae4a03d5b2cb7482c57c50da3b9d8b98d307bf9
Joshua Anderson <joshuaanderson2024@gmail.com>
2022-07-12 01:40:56 -0400

    Added AllPossibleScores which is useful for checking scores where cards have multiple values

---
+++ AllPossibleScores.py
@@ -0,0 +1,35 @@
+import itertools
+
+def CalcAllScoresFaster(rules, cards):
+    cards_values = [rules[card] for card in cards]
+    
+    score = sum([value for value in cards_values if type(value) is int])
+    poss_perms = [value for value in cards_values if type(value) is tuple]
+
+    result = [score + sum(combo) for combo in itertools.product(*poss_perms)]
+
+    return list(set(result))
+
+
+#based on the 1-13 value for the cards, and the rules following blackjack rules.
+#gives a full list of all the possible score permutations
+
+test_cards = [1, 11]
+
+rules = {
+    1: (1,11),
+    2: (2),
+    3: (3),
+    4: (4),
+    5: (5),
+    6: (6),
+    7: (7),
+    8: (8),
+    9: (9),
+    10: (10),
+    11: (10),
+    12: (10),
+    13: (10)
+}
+
+print("All Scores: ", CalcAllScoresFaster(rules, test_cards))
