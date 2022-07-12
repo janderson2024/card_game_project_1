f7e437c108c5fcebdb5bb065deb00caa2896836a
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2022-07-11 20:36:23 -0700

    Updated Card
    Updated card class and added main for testing

--- Card.py
+++ Card.py
@@ -1,13 +1,18 @@
+
 class Card:
+    displayable = True
     suits = ['\u2666', '\u2665', '\u2663', '\u2660']
-    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
     def __str__(self):
-        return f"{Card.ranks[self.rank]}{Card.suits[self.suit]}"
+        return f"[{Card.suits[self.suit]}{Card.ranks[self.rank]}]" if self.displayable else "[  ]"
+
+    def set_display(self, display):
+        self.displayable = display
 
     def __lt__(self, other):
         if self.rank == other.rank:
---
+++ Main.py
@@ -0,0 +1,6 @@
+from Card import Card
+
+if __name__ == '__main__':
+    card = Card(3, 12)
+    card.set_display(False)
+    print(card)
