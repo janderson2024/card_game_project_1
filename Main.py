4005d74e6551b829af0adac9797d94795a28db4f
Joshua Anderson <joshuaanderson2024@gmail.com>
2022-07-18 21:52:21 -0400

    removed the testing code in main so that we can start working on a game loop

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
 from Card import Card
 from Card_List import Card_List
 
-
-def game_testing():
+if __name__ == '__main__':
     #TODO: make the import of the game not hardcoded
     #ask what game to play
-
     from games import crazy8
 
     game = crazy8.crazy8()
@@ -24,27 +22,3 @@ def game_testing():
     print(game.stock)
     for player in player_list:
         print(player)
-
-def card_testing():
-    card_list = Card_List([])
-    card_list.set_52()
-    print(card_list)
-    card_list.set_54()
-    print(card_list)
-    card_list.shuffle()
-    print(card_list)
-    card = Card(0, 0)
-    card_list.rem_card(card)
-    print(card_list)
-    cards = Card_List([Card(3, x) for x in range(13)])
-    card_list.rem_cards(cards)
-    print(card_list)
-    print(len(card_list), card_list.num_cards_left())
-    cards = card_list.get_card_list()
-    print(cards)
-
-
-
-
-if __name__ == '__main__':
-    game_testing()
