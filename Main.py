868e0ac4a6e737b5149dc9793736a838e914b080
Joshua Anderson <joshuaanderson2024@gmail.com>
2022-07-18 23:05:35 -0400

    Working on crazy 8 logic

--- Main.py
+++ Main.py
@@ -1,17 +1,18 @@
+from Card import Card
+
 if __name__ == '__main__':
     #TODO: make the import of the game not hardcoded
     #ask what game to play
     from games import crazy8
-
     game = crazy8.crazy8()
+
+
 
     player_list = []
 
     for num in range(4):
         player_list.append(game.getNewPlayer(str(num)))
 
-    #print(game.stock)
-    #print(player_list[0])
 
     game.deal_to_players(player_list)
     print("shuffle and deal to players: ")
@@ -19,3 +20,21 @@ if __name__ == '__main__':
     print(game.stock)
     for player in player_list:
         print(player)
+
+    game.start_game()
+
+    print(game.discard)
+
+    test_card = Card(1, 1, displayable=True)
+    print(test_card)
+
+    print("is card valid: ", game.is_card_valid(test_card))
+
+    game.on_card_played(test_card)
+
+    print(game.discard)
+
+    print("------")
+    print(player_list[0])
+
+    print(game.check_player_win(player_list[0]))
--- games/crazy8.py
+++ games/crazy8.py
@@ -2,48 +2,50 @@ import sys
 sys.path.insert(0, '..')
 
 from Game import Game
-from card_list.Card_List import Draw, Discard
+from card_list.Draw import Draw
+from card_list.Discard import Discard
 
 class crazy8(Game):
 	DEAL_COUNT = 5
 
 	round_counter = 1
 
-	discard = Discard([])
+	
 
 	def __init__(self):
 		self.stock = Draw([])
 		self.stock.set_52_lo()
 		self.stock.shuffle()
+		self.discard = Discard([])
 
 	def start_game(self):
-		discard.add_card(stock.pop_card())
+		self.discard.add_card(self.stock.pop_card())
 		pass
 
 	def deal_to_players(self, players):
 		for _ in range(self.DEAL_COUNT):
 			for player in players:
-				player.deck.append(stock.pop_card()) #TODO create a getTopCard and popTopCard
+				player.deck.append(self.stock.pop_card()) #TODO create a getTopCard and popTopCard
 
 	def is_card_valid(self, card):
 		if card.rank == 8:
 			return True
 
-		top_card = discard.get_top_card
+		top_card = self.discard.get_top_card()
 
 		if card.suit == top_card.suit:
 			return True
 		if card.rank == top_card.rank:
 			return True
 
-		return True
+		return False
 
 	def on_card_played(self, card):
-		discard.add_card(card)
+		self.discard.add_card(card)
 
 
 	def check_player_win(self, player):
-		if player.get_number_of_cards() == 0
+		if player.get_number_of_cards() == 0:
 			return True
 		else:
 			return False
