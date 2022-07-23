56a122ec323df95eb71b14483dc0ce9ffcbda25d
DickeyT <dickeyt83@gmail.com>
2022-07-22 20:19:50 -0500

    updated pygame card tester

--- pygame_card_tester.py
+++ pygame_card_tester.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-from Card import Card as Card
+from CardLib.Card import Card as Card
 
 pygame.init()
 fps = 60
@@ -22,7 +22,7 @@ pygame.display.set_caption('Card Tester')
 font = pygame.font.SysFont('Arial', 20)
 
 
-card_image = pygame.image.load(f'./card_images/{card1.image}')
+card_image = pygame.image.load(f'CardLib/card_images/standard/{card1.image}')
 DEFAULT_IMAGE_SIZE = (250, 363)
 card_image = pygame.transform.scale(card_image, DEFAULT_IMAGE_SIZE)
 
@@ -78,7 +78,7 @@ class Button():
 def flip_card():
     global card_image, rank, suit, suit_text, color, image_display, displayable, str_display
     card1.set_display()
-    card_image = pygame.image.load(f'./card_images/{card1.image}')
+    card_image = pygame.image.load(f'CardLib/card_images/standard/{card1.image}')
     DEFAULT_IMAGE_SIZE = (250, 363)
     card_image = pygame.transform.scale(card_image, DEFAULT_IMAGE_SIZE)
     rank = font.render(f"Card Rank: {card1.rank}", True, white)
@@ -96,7 +96,7 @@ def change_card():
     card_split = str(new_card).split(",")
     new_suit, new_rank = card_split[0], card_split[1]
     card1 = Card(new_suit, new_rank)
-    card_image = pygame.image.load(f'./card_images/{card1.image}')
+    card_image = pygame.image.load(f'CardLib/card_images/standard/{card1.image}')
     DEFAULT_IMAGE_SIZE = (250, 363)
     card_image = pygame.transform.scale(card_image, DEFAULT_IMAGE_SIZE)
     rank = font.render(f"Card Rank: {card1.rank}", True, white)
