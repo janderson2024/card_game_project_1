56d1a6e3de4675cc620b7a489abf8437f87c4fd3
Wehde <kewehde@dmacc.edy>
2022-07-25 19:53:39 -0500

    Added Type Hints

--- CardLib/cardlists/CardList.py
+++ CardLib/cardlists/CardList.py
@@ -4,44 +4,45 @@ from CardLib import Card
 
 
 class CardList:
-	
-    def __init__(self, cards=[]):
+    def __init__(self, cards: [Card] = None):
+        if cards is None:
+            cards = []
         self.card_list = cards
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.card_list)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return " ".join([str(card) for card in self.card_list])
 
-    def add_card(self, card):
+    def add_card(self, card: Card):
         self.card_list.append(card)
 
-    def get_card_at(self, index):
+    def get_card_at(self, index: int) -> Card:
         return self.card_list[index]
 
-    def add_cards(self, cards):
+    def add_cards(self, cards: [Card]):
         for card in cards:
             self.card_list.append(card)
 
-    def rem_card(self, card):
+    def rem_card(self, card: Card):
         self.card_list.remove(card)
 
-    def rem_cards(self, cards):
+    def rem_cards(self, cards: 'CardList'):
         for card in cards.card_list:
             self.card_list.remove(card)
 
     def rem_all_cards(self):
         self.card_list = []
 
-    def num_cards_left(self):
+    def num_cards_left(self) -> int:
         return len(self.card_list)
 
     def shuffle(self):
         random.shuffle(self.card_list)
 
-    def get_card_list(self):
+    def get_card_list(self) -> [Card]:
         return self.card_list
 
-    def is_card_in(self, card):
+    def is_card_in(self, card) -> bool:
         return card in self.card_list
