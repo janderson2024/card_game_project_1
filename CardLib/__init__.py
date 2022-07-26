1b4bc7a6a266260883c7fd903458478e34f53338
Wehde <kewehde@dmacc.edy>
2022-07-25 22:24:12 -0500

    Actually used proper suits instead of suit text

--- CardLib/__init__.py
+++ CardLib/__init__.py
@@ -27,10 +27,11 @@ __all__ = [
     "fill_deck_standard_54"
 ]
 
-DIAMOND = 'diamonds'
-SPADE = 'spades'
-HEART = 'diamonds'
-CLUB = 'clubs'
+DIAMOND = '\u2666'
+HEART = '\u2665'
+CLUB = '\u2663'
+SPADE = '\u2660'
+
 JOKER = '*'
 
 
