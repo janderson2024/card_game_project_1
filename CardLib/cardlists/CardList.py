4c2e3819a032e62f928dc84403a7c133c663a466
Joshua Anderson <joshuaanderson2024@gmail.com>
2022-08-02 01:47:48 -0400

    Made CardList iterable. (also now get highest card works on a CardList)

--- CardLib/cardlists/CardList.py
+++ CardLib/cardlists/CardList.py
@@ -13,6 +13,9 @@ class CardList:
     def __str__(self) -> str:
         return " ".join([str(card) for card in self.card_list])
 
+    def __iter__(self):
+        yield from self.card_list
+
     def add_card(self, card: Card):
         self.card_list.append(card)
 
