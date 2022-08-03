6ab36929ce7abe6e12146dcee1c83ae5a6f3a3fc
Wehde <kewehde@dmacc.edy>
2022-08-02 21:25:09 -0500

    Added type hints

--- CardLib/Player.py
+++ CardLib/Player.py
@@ -1,38 +1,38 @@
 import random
 
-from CardLib import Hand
+from CardLib import Hand, Card
 
 
 class Player:
-    def __init__(self, label, is_ai=False):
+    def __init__(self, label: str, is_ai: bool = False):
         self.is_ai = is_ai
         self.hand = Hand([])
         self.label = label
         self.win_count = 0
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.label + ': ' + str(self.hand)
 
-    def get_label(self):
+    def get_label(self) -> str:
         return self.label
 
-    def get_win_count(self):
+    def get_win_count(self) -> int:
         return self.win_count
 
     def round_winner(self):
         self.win_count = self.win_count + 1
 
-    def get_amount_of_cards(self):
+    def get_amount_of_cards(self) -> int:
         return len(self.hand)
 
-    def add_card_to_hand(self, card):
+    def add_card_to_hand(self, card: Card):
         self.hand.add_card(card)
 
-    def add_cards_to_hand(self, cards):
+    def add_cards_to_hand(self, cards: [Card]):
         for card in cards:
             self.hand.add_card(card)
 
-    def play_card(self, number=-1):
+    def play_card(self, number: int = -1) -> Card:
         if self.is_ai:
             number = random.randint(0, len(self.hand) - 1)
         temp_card = self.hand.get_card_at(number)
