1d4e69a63ba5980d92c4c5e1a109cb14263218a4
Joshua Anderson <joshuaanderson2024@gmail.com>
2022-08-16 22:26:20 -0400

    refactored main to check for the --create-new-game flag, and created an example game that will be copied into the created game

---
+++ CardLib/example_game.py
@@ -0,0 +1,65 @@
+import CardLib
+
+
+class {GAME_NAME}:
+
+    def __init__(self):
+        #Init all of your game data here
+        pass
+
+    def get_ai_turn(self, player):
+        #function to run if the player is an AI
+
+    def get_user_turn(self, player):
+        #function to run if the player is a user
+        pass
+
+    def on_player_turn(self, player):
+        if player.is_ai:
+            played_card = self.get_ai_turn(player)
+        else:
+            played_card = self.get_user_turn(player)
+
+        return played_card
+
+
+def main_loop(game, player_list):
+    playing = True
+    while playing:
+        #Start of game code goes here
+
+        #example loop of a simple "game loop"
+        current_player = 0
+        playing_round = True
+        while playing_round:
+
+            #gets the current Player object
+            player = player_list[current_player]
+
+            # code for each player's turn goes here
+            
+            #cycles to the next player
+            current_player = (current_player + 1) % 4
+
+        #asks the user to play the game again
+        action, _ = CardLib.get_user_input(["yes", "no"], "Do you want to play again?")
+        if action == "no":
+            playing = False
+
+    #only prints once the user is done playing
+    print("Thank you for playing!")
+
+
+def start_game():
+    game = {GAME_NAME}()
+
+    print("Lets play {GAME_NAME}!")
+
+    player_list = [CardLib.Player("Player")]
+
+    PLAYER_COUNT = 4
+
+    for num in range(1, game.PLAYER_COUNT):
+        player_list.append(CardLib.Player("AI #" + str(num), is_ai=True))
+
+    main_loop(game, player_list)
--- Main.py
+++ Main.py
@@ -1,27 +1,48 @@
 import importlib
 import os
+import sys
 
 GAMES_FOLDER = "games/"
-
 
 def get_all_game_files() -> [str]:
     files_in_games = os.listdir(GAMES_FOLDER)
     games = [file[:-3] for file in files_in_games if file.endswith(".py")]
     return games
 
+def play_games():
+    from CardLib import get_user_input
 
-from CardLib import get_user_input
+    all_games = get_all_game_files() + ["exit"]
+    user_input, _ = get_user_input(all_games, "Please select which game you would like to play")
 
-all_games = get_all_game_files() + ["exit"]
+    if user_input == "exit":
+        exit()
+    else:
+        module_to_import = "games." + user_input
+        # module_to_import = "games.crazy8" FOR TESTING
 
-user_input, _ = get_user_input(all_games, "Please select which game you would like to play")
+        game = importlib.import_module(module_to_import)
+        game.start_game()
 
-if user_input == "exit":
-    exit()
+
+
+def create_game(game_name):
+    with open(GAMES_FOLDER + str(game_name) + ".py", "w") as file:
+        with open("CardLib/example_game.py", "r") as example_game:
+            for line in example_game:
+                line = line.replace("{GAME_NAME}", str(game_name))
+
+                file.write(line)
+    
+    print("Created the ", game_name, "in the games/ folder")
+
+
+args = sys.argv[1:]
+
+if len(args) == 0:
+    play_games()
+elif len(args) == 2 and args[0] == "--create-new-game":
+    create_game(args[1])
 else:
-    module_to_import = "games." + user_input
-    # module_to_import = "games.crazy8" FOR TESTING
-
-    game = importlib.import_module(module_to_import)
-
-    game.start_game()
+    print("improper argument.")
+    print("Arguements: --create-new-game [GAME NAME]")
