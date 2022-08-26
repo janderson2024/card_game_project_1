3382dc081efd9faaf3b0e3394c94b41a30127671
Joshua Anderson <joshuaanderson2024@gmail.com>
2022-08-26 17:36:34 -0400

    Changed button and label to go "text", x=0,y=0

--- CardLib/gui/GuiButton.py
+++ CardLib/gui/GuiButton.py
@@ -2,7 +2,7 @@ import CardLib
 
 class GuiButton:
 
-	def __init__(self, x, y, button_text, text_color=(255,255,255), button_background=(100,100,100)):
+	def __init__(self, button_text, x=0, y=0, text_color=(255,255,255), button_background=(100,100,100)):
 		self.rendered_text = CardLib.gui.create_label(button_text, text_color, button_background)
 		(width, height) = self.rendered_text.get_size()
 		self.gui_obj = CardLib.gui.GuiObject(x, y, width, height, self.gui_draw)
--- CardLib/gui/GuiLabel.py
+++ CardLib/gui/GuiLabel.py
@@ -2,7 +2,7 @@ import CardLib
 
 class GuiLabel:
 
-	def __init__(self, x, y, to_print):
+	def __init__(self, to_print, x=0, y=0):
 		self.rendered_text = CardLib.gui.create_label(to_print, (255,255,255))
 		(width, height) = self.rendered_text.get_size()
 		self.gui_obj = CardLib.gui.GuiObject(x, y, width, height, self.gui_draw)
