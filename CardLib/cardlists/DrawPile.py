fcb80d2c3bc281c2a2f73bbfaf4ace9bbfe4fa1a
ChrisSantillan <csantillan1@dmacc.edu>
2022-08-25 21:03:59 -0500

    Added background image to the draw pile

--- CardLib/cardlists/DrawPile.py
+++ CardLib/cardlists/DrawPile.py
@@ -5,6 +5,9 @@ class DrawPile(CardList):
     def _init_gui(self, x, y):
         self.gui_obj = CardLib.gui.GuiObject(x, y, 70, 90, self.gui_draw)
         self.background_rect = CardLib.gui.create_rect(self.gui_obj.x, self.gui_obj.y, self.gui_obj.width, self.gui_obj.height)
+        self.PATH_TO_IMG_DIR = "CardLib/card_images/standard/"
+        back_img = self.PATH_TO_IMG_DIR + "card_back.png"
+        self.back_img = CardLib.gui.create_img(back_img, self.gui_obj.width, self.gui_obj.height)
 
     def pop_card(self, display: bool = True) -> Card:
         card = self.card_list.pop()
@@ -16,4 +19,5 @@ class DrawPile(CardList):
 
     def gui_draw(self):
         CardLib.gui.draw_rect((0,0,0), self.background_rect)
+        CardLib.gui.draw_img(self.back_img, self.gui_obj.x, self.gui_obj.y)
         #whatever should be drawn on top of the background rect should go here
