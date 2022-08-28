d9bf42c914524af86c38bc2298b4767ab3519302
Wehde <kewehde@dmacc.edy>
2022-08-28 18:04:19 -0500

    Changed type hint in get_all_valid_cards to be callable to show that it is a function

--- CardLib/cardlists/Hand.py
+++ CardLib/cardlists/Hand.py
@@ -9,7 +9,7 @@ class Hand(CardLib.CardList):
     def sort_by_rank(self):
         self.card_list = sorted(self.card_list, key=lambda card: (0 - card.value, card.suit))
 
-    def get_all_valid_cards(self, game_valid_cards: [CardLib.Card]) -> CardLib.CardList:
+    def get_all_valid_cards(self, game_valid_cards: callable) -> CardLib.CardList:
         return CardLib.CardList(list(filter(game_valid_cards, self.card_list)))
 
     #does not implement _init_gui, update_card_pos, or gui_draw as they are the same as CardList
